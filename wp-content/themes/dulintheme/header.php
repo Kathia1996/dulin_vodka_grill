76d2df55fb6ab75e2feb36758956734b21d0b6e9
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-06-25 15:21:50 +0300

    stretch logo

--- wp-content/themes/dulintheme/header.php
+++ wp-content/themes/dulintheme/header.php
@@ -36,8 +36,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main__head">
 			<h1 class="site-title__head">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<img class="site-title__head-img" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="logo" width="493">
+				<a class="site-title__head-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 				</a>
 			</h1>
 
