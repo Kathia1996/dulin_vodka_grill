d49295c6b98a252c7520cd792db82012b9e244da
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-06-27 11:40:25 +0300

    Добавил футер

--- wp-content/themes/dulintheme/footer.php
+++ wp-content/themes/dulintheme/footer.php
@@ -14,7 +14,24 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
-				
+				<div class="wrapper">
+					<div class="wrapper__inner">
+						<div class="wrapper__inner">
+							<div class="col-fl-left">
+								<strong>ул. Льва толстого 57</strong>
+								<strong>044 538 18 88</strong>
+							</div>
+							<div class="col-fl-center">
+								<strong>ул. Льва толстого 57</strong>
+								<strong>044 538 18 88</strong>
+							</div>
+							<div class="col-fl-right">
+								<a href="#" class="social-btn social-btn__facebook"></a>
+								<a href="#" class="social-btn social-btn__instagram"></a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
